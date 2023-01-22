8f12753f6649035b94219508363db6bd0f54eafb
joshua <joshua@guerilla360.com>
2023-01-22 20:05:55 +0800

    First Commit

---
+++ src/com/codewithjoshua/Main.java
@@ -0,0 +1,33 @@
+package com.codewithjoshua;
+
+
+import java.text.NumberFormat;
+import java.time.Period;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main{
+    public static void main(String[] args){
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal : ");
+        Integer principal = scanner.nextInt();
+        System.out.print("Annual Rate : ");
+        Double annualInterestRate = scanner.nextDouble();
+        System.out.print("Period (Years): ");
+        Byte period = scanner.nextByte();
+
+        Double monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        int numberOfPayments = period * MONTHS_IN_YEAR;
+
+        Double Mortgage = principal *
+                ( monthlyInterestRate  * Math.pow(1 + monthlyInterestRate, numberOfPayments))
+                / (Math.pow(1+ monthlyInterestRate, numberOfPayments) -1);
+
+        System.out.println("Monthly interest Rate :" + monthlyInterestRate);
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(Mortgage));
+    }
+}
